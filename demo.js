b9a0737c38b0955255fd30c49f5886715d1ab81e
Hack Reactor Students <accounts@hackreactor.com>
2016-04-30 17:26:18 -0700

    Currently on part two of d3 bar graphs tutorial

--- demo.js
+++ demo.js
@@ -1,38 +1,80 @@
 var body = d3.select('body');
 var svg = d3.select('body').select('svg');
 
+var datasetTest = [{item: 'cats', value: 20}, {item:'dogs', value: 25}, {item: 'parakeet', value: 45}, {item: 'serval', value: 50}];
+
+// var data = [4, 8, 15, 16, 23, 42];
+
+var width = 420;
+var barHeight = 20;
+
+var x = d3.scale.linear()
+  .range([0, 500]);
+
+var chart = d3.select('.chart')
+  .attr('width', width)
+
+var type = function(d) {
+  d.Number_of_Patients = +d.Number_of_Patients;
+  return d;
+}
+
+d3.csv('data2.csv', type, function(error, data) {
+  x.domain([0, d3.max(data, function(d) { return d.Number_of_Patients; })]);
+
+  chart.attr('height', barHeight * data.length);
+
+  var bar = chart.selectAll('g')
+      .data(data)
+    .enter().append('g')
+      .attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'; });
+
+  bar.append('rect')
+    .attr('width', function (d) { return x(d.Number_of_Patients); })
+    .attr('height', barHeight - 2)
+    .style('fill', 'steelblue');
+
+  bar.append('text')
+    .attr('x', function(d) { return 2; }) //-3 is for spacing on the right side
+    .attr('y', barHeight / 2) /// 2 is for spacing above and below
+    .attr('dy', '.35em')
+    .text(function(d) { return d.Number_of_Patients; });
+  
+});
+
 
 var updateBarGraph = function(data) {
-  var graphStats = body.select('.chart').selectAll('.barStat');
+  // var graphStats = body.select('.chart').selectAll('.barStat');
 
   var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
+    .domain([0, d3.max(data, function(d) { return d.value; })])
     .range([0, 500]);
 
     //update graphStats
-    graphStats
-      .data(data)
-          .attr('width', function (d) { return x(d) + 'px' })
-          .text(function(d) { return d });
+    var graphStats = body.select('.chart').selectAll('.barStat')
+      .data(data, 
+        function(d) { return d.item; })
+      .attr('width', function (d) { return x(d.value) + 'px' })
+      .text(function(d) { return d.value });
 
     //create new graphStats
     graphStats
-      .data(data)
+      // .data(data)
         .enter()
         .append('div')
           .attr('class', 'barStat')
           // .style('height', '10px')
-          .style('width', function (d) { return x(d) + 'px' })
-          .style('background-color', 'blue')
-          .text(function(d) { return d });
+          .style('width', function (d) { return x(d.value) + 'px' })
+          .style('background-color', 'steelblue')
+          .text(function(d) { return d.value });
     //delete unused graphStats
     graphStats
-      .data(data)
+      // .data(data)
         .exit()
         .remove()
 }
 
-updateBarGraph([20, 25, 45, 50]);
+// updateBarGraph(datasetTest);
 
 var createCircle = function(d, color) {
   var r = d / 2;
